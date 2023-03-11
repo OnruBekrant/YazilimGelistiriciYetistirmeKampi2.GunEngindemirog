82b87a154e690fc31965fa5e8148307c45cdd354
Onru Bekrant <cerimgt@gmail.com>
2023-03-11 23:11:57 +0300

    11.03.2023 23:11

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,17 @@
+﻿using GenericsIntro;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        MyList<string> isimler = new MyList<string>();
+        isimler.Add("Engin");
+        Console.WriteLine(isimler.Length);
+        isimler.Add("Kerem");
+        Console.WriteLine(isimler.Length);
+        foreach (var isim in isimler.Items)
+        {
+            Console.WriteLine( isim);
+        }
+    }
+}
